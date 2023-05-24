785392ab9a1bbcd8f99a72a4a331928c194c499a
daniliard <55604849+daniliard@users.noreply.github.com>
2023-05-24 20:30:47 +0300

    Add files via upload

---
+++ uniqueChars.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введiть перше слово:");
+        string firstWord = Console.ReadLine();
+
+        Console.WriteLine("Введiть друге слово:");
+        string secondWord = Console.ReadLine();
+
+        var uniqueChars = firstWord.Except(secondWord).Concat(secondWord.Except(firstWord));
+
+        Console.WriteLine("Унiкальнi символи: ");
+        foreach (char c in uniqueChars)
+        {
+            Console.Write(c);
+        }
+
+        Console.ReadKey();
+    }
+}
